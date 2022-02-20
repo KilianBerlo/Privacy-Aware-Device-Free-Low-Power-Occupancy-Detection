20533422790bee8f3c240cfe307eb6bcdf13c5c7
kilian <k.v.berlo@outlook.com>
2022-02-20 21:39:47 +0100

    some changes to MQTT

--- mQTTDevice/mQTT_conn.cs
+++ mQTTDevice/mQTT_conn.cs
@@ -1,9 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// This application uses the Azure IoT Hub device SDK for .NET
-// For samples see: https://github.com/Azure/azure-iot-sdk-csharp/tree/main/iothub/device/samples
-
 using Microsoft.Azure.Devices.Client;
 using System;
 using System.Collections.Generic;
@@ -28,18 +25,12 @@ namespace mQTTDevice
         public double[] coordinates { get; set; }
     }
 
-    /// <summary>
-    /// This sample illustrates the very basics of a device app sending telemetry. For a more comprehensive device app sample, please see
-    /// <see href="https://github.com/Azure-Samples/azure-iot-samples-csharp/tree/main/iot-hub/Samples/device/DeviceReconnectionSample"/>.
-    /// </summary>
     internal class Program
     {
         private static DeviceClient s_deviceClient;
         private static readonly TransportType s_transportType = TransportType.Mqtt;
 
         // The device connection string to authenticate the device with your IoT hub.
-        // Using the Azure CLI:
-        // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
         private static string s_connectionString = "HostName=geojson-ticket-hub.azure-devices.net;DeviceId=tudelft_device001;SharedAccessKey=6dCj+Nr3TqWAuuJ303DbpEqOheoXWNKi60ixbG3Dx2Q=";
 
         private static async Task Main(string[] args)
@@ -64,13 +55,6 @@ namespace mQTTDevice
 
             // Run the telemetry loop
             await SendDeviceToCloudMessagesAsync(cts.Token);
-
-            // SendDeviceToCloudMessagesAsync is designed to run until cancellation has been explicitly requested by Console.CancelKeyPress.
-            // As a result, by the time the control reaches the call to close the device client, the cancellation token source would
-            // have already had cancellation requested.
-            // Hence, if you want to pass a cancellation token to any subsequent calls, a new token needs to be generated.
-            // For device client APIs, you can also call them without a cancellation token, which will set a default
-            // cancellation timeout of 4 minutes: https://github.com/Azure/azure-iot-sdk-csharp/blob/64f6e9f24371bc40ab3ec7a8b8accbfb537f0fe1/iothub/device/src/InternalClient.cs#L1922
             await s_deviceClient.CloseAsync();
 
             s_deviceClient.Dispose();
@@ -109,16 +93,9 @@ namespace mQTTDevice
         // Async method to send simulated telemetry
         private static async Task SendDeviceToCloudMessagesAsync(CancellationToken ct)
         {
-            // // Initial telemetry values
-            // double minTemperature = 20;
-            // double minHumidity = 60;
-            // var rand = new Random();
 
             while (!ct.IsCancellationRequested)
             {
-                double currentTemperature = 35;//minTemperature + rand.NextDouble() * 15;
-                // double currentHumidity = minHumidity + rand.NextDouble() * 20;
-
                 // Create JSON message
                 string messageBody = JsonSerializer.Serialize(
                     new
@@ -127,18 +104,12 @@ namespace mQTTDevice
                         id = "54:ee:75:57:b8:59",
                         properties = new deviceProps {accuracy = 100, battery = "85%", client = "TUDelft", color = "red", dateTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ")},
                         geometry = new dataset {type = "Point", coordinates = new[] {5.458188056945801, 51.41125257942182 }}
-                        // temperature = currentTemperature,
-                        // humidity = currentHumidity
                     });
                 using var message = new Message(Encoding.ASCII.GetBytes(messageBody))
                 {
                     ContentType = "application/json",
                     ContentEncoding = "utf-8",
                 };
-
-                // Add a custom application property to the message.
-                // An IoT hub can filter on these properties without access to the message body.
-                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
 
                 // Send the telemetry message
                 await s_deviceClient.SendEventAsync(message);
--- mQTT_conn.py
+++ mQTT_conn.py
@@ -2,35 +2,24 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import asyncio
 from datetime import datetime
-import random
 from azure.iot.device.aio import IoTHubDeviceClient
 from azure.iot.device import Message
-
-# The device connection authenticates your device to your IoT hub. The connection string for 
-# a device should never be stored in code. For the sake of simplicity we're using an environment 
-# variable here. If you created the environment variable with the IDE running, stop and restart 
-# the IDE to pick up the environment variable.
-#
-# You can use the Azure CLI to find the connection string:
-#     az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
 
 CONNECTION_STRING = "HostName=geojson-ticket-hub.azure-devices.net;DeviceId=tudelft_device001;SharedAccessKey=6dCj+Nr3TqWAuuJ303DbpEqOheoXWNKi60ixbG3Dx2Q="
 
 # Define the JSON message to send to IoT Hub.
-TEMPERATURE = 20.0
-HUMIDITY = 60
+type = "Feature"
+id = "54:ee:75:57:b8:59"
+accuracy = 100
+battery = "85%"
+clientel = "TUDelft"
+color = "red"
+typeGeom = "Point"
+coordinates = [51.43, 24.31]
+# MSG_TXT2 = '{{"Accuracy": {acc}, "Battery": {bat}, "Color": {col}, "id": {idd}, "Latitude": {lat}, "Longitude": {long}}}'
 PROPERTY_TXT = '{{"accuracy": {accuracy}, "battery": {battery}, "client": {client}, "color": {color}, "dateTime": {dateTime}}}'
 GEOM_TXT = '{{"type": {typeGeom}, "coordinates": {coordinates}}}'
 MSG_TXT = '{{"type": {type},"id": {id}, "properties": {properties}, "geometry": {geometry}}}' 
-
-#   "geometry": {
-#     "type": "Point",
-#     "coordinates": [
-#       5.458188056945801,
-#       51.41125257942182
-#     ]
-#   }
-# }
 
 async def run_telemetry_sample(client):
     # This sample will send temperature telemetry every second
@@ -40,30 +29,14 @@ async def run_telemetry_sample(client):
 
     while True:
         # Build the message with simulated telemetry values.
-        # temperature = TEMPERATURE + (random.random() * 15)
-        # humidity = HUMIDITY + (random.random() * 20)
-
-        type = "Feature"
-        id = "54:ee:75:57:b8:59"
-        accuracy = 100
-        battery = "85%"
-        clientel = "TUDelft"
-        color = "red"
         dateTime = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        typeGeom = "Point"
-        coordinates = [51.43, 24.31]
 
         properties = PROPERTY_TXT.format(accuracy=accuracy, battery=battery, client=clientel, color=color, dateTime=dateTime)
         geometry = GEOM_TXT.format(typeGeom=typeGeom, coordinates=coordinates)
 
-        msg_txt_formatted = MSG_TXT.format(type=type, id=id, properties=properties, geometry=geometry) #temperature=temperature, humidity=humidity, 
+        msg_txt_formatted = MSG_TXT.format(type=type, id=id, properties=properties, geometry=geometry)
+        # msg_txt_formatted = MSG_TXT2.format(acc=accuracy, bat=battery, col=color, idd=id, lat=coordinates[0], long=coordinates[1])
         message = Message(msg_txt_formatted)
-        # Add a custom application property to the message.
-        # An IoT hub can filter on these properties without access to the message body.
-        # if temperature > 30:
-        #     message.custom_properties["temperatureAlert"] = "true"
-        # else:
-        #     message.custom_properties["temperatureAlert"] = "false"
 
         # Send the message.
         print("Sending message: {}".format(message))
@@ -76,8 +49,7 @@ def main():
     print ("IoT Hub Quickstart #1 - Simulated device")
     print ("Press Ctrl-C to exit")
 
-    # Instantiate the client. Use the same instance of the client for the duration of
-    # your application
+    # Instantiate the client
     client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
     loop = asyncio.get_event_loop()
